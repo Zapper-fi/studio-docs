5e9db3a06138003b74714366e58a201974212974
Sam Tolomei <43358952+SamIam-0x@users.noreply.github.com>
2022-05-13 12:27:40 -0700

    Update index.tsx

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,27 +10,32 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: "Fully TypeScript",
+    title: "Keep Your Users Happy",
     Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
-    description: <>Enjoy a full codebase written completely in TypeScript.</>,
+    description: (
+    <>
+      Over 1,000,0000 users manage their portfolio with Zapper each month, 
+      and they want to see all their balances in one place. By integrating your app, users will be able to monitor and update their portfolio in Zapper
+    </>
+    ),
   },
   {
-    title: "Rapid Scaffolding",
+    title: "Get More Users",
     Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
-        Studio includes scaffolding helpers so you can quickly integrate
-        relevant dApp information directly into Zapper.
+         Zapper drives discovery, through our investment discovery pages, app discovery, 
+         looking at other users’ portfolios and through apps using Zapper’s balance data via API
       </>
     ),
   },
   {
-    title: "Recipes and Helpers",
+    title: "Rapid Scaffolding, Recipes and Helpers",
     Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
-        Studio has an extensive recipe book (this site!) and helpers so you can
-        spend less time integrating and more time building your product.
+        Studio includes scaffolding helpers so you can quickly integrate relevant 
+        app information directly into Zapper, and has extensive recipes (this site!) to help you get it done quickly
       </>
     ),
   },
