ac256e6dac8aee5224aa4e9ab4eb71cda1948350
Christophe Divina <christophedivina@gmail.com>
2023-04-04 12:44:56 +0200

    Render static page

---
+++ src/pages/documentation/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Layout from '@theme/Layout';
+import { RedocStandalone } from 'redoc';
+
+const DocumentationPage = () => {
+    return (
+        <Layout>
+            <RedocStandalone specUrl="https://api.zapper.xyz/api/json" />
+        </Layout>
+    )
+}
+
+export default DocumentationPage;
